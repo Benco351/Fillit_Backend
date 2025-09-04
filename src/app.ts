43671fe7a3a8717ac8fc7db1cc3ecdd1bf62d143
David Weiss <davidwe@mta.ac.il>
2025-09-04 14:11:10 +0300

    someone messed up with cors

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ import { tokenAuthentication } from './middlewares/authMiddleware';
 const app: Application = express();
 
 const FRONTEND_URL = ["https://www.fillitshifits.com","https://fillitshifits.com"];
-const whitelist = [FRONTEND_URL, "localhost:3000", "http://localhost:3000"];
+const whitelist = [FRONTEND_URL, "localhost:3000", "http://localhost:3000", "https://www.fillitshifits.com"];
 
 const corsOptions: cors.CorsOptions = {
   // Only allow your SPA origin (and also allow tools like curl with no Origin header)
